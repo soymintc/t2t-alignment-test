27cb716ac546caed530a0fd9bb3b8cf8db3af3b8
Seongmin Choi <soymintc@gmail.com>
2024-02-06 12:04:34 -0500

    add multiqc, add savana

--- pipeline/run_snakemake.sh
+++ pipeline/run_snakemake.sh
@@ -2,6 +2,8 @@ cmd="snakemake"
 cmd="$cmd -p"
 cmd="$cmd -c60"
 cmd="$cmd --jobs 60"
+cmd="$cmd --use-singularity"
+cmd="$cmd --singularity-args \"--bind /rtsess01 --bind /home\""
 # cmd="$cmd --dry-run"
 # cmd="$cmd --allowed-rules bwa_mem"
 
